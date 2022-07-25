aa0e463d751a1fed57956281c78c4263796bbc2f
taniyaraina <rainataniya668@gmail.com>
2022-07-25 18:36:43 +0530

    fixed contactinfo

--- src/components/elements/ContactInfo.js
+++ src/components/elements/ContactInfo.js
@@ -16,13 +16,19 @@ const ContactInfo = ({ data }) => {
   return (
     <Section className="column card has-text-white">
       {data.contact &&
-        data.contact.map((item) => (
+        data.contact.map((item, index) => (
           <div className="card-content pt-0 pb-4">
             <h5 className="has-text-weight-semibold py-3">{item.title}</h5>
             <div className="content">
-              <p className="has-text-weight-bold">{item.info}</p>
+              {item.href ? (
+                <a className="has-text-weight-bold" href={item.href}>
+                  {item.info}
+                </a>
+              ) : (
+                <p className="has-text-weight-bold">{item.info}</p>
+              )}
             </div>
-            <div className="line" />
+            {index + 1 !== data.contact.length && <div className="line" />}{' '}
           </div>
         ))}
     </Section>
